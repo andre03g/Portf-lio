068c071b0c92e1a9e00eb34a8300fe5980f335db
andre03g <brilhantequintas2@gmail.com>
2024-02-17 21:13:29 -0300

    🚀 feat: criando portfolio estilizando e adcionando inteligencia

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+  var typed = new Typed(".typing", {
+    strings:["André Brilhante de Souza Quintas"],
+    typeSpeed:200,
+    backSpeed:60,
+    loop:false
+  });
+
+});
